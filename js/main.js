061fb0956efa11877ae655ba59532c59ec6d2e99
William Dias <smdwilliam89@gmail.com>
2022-12-11 22:24:57 +0000

    Eventos + Variaveis + Funcoes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-  console.log('Bem vindo ao meu site');
+  $('#form-login').submit(function (e) {
+    e.preventDefault();
+
+    var emailDigitado = $('#inputEmail').val();
+    var senhaDigitada = $('#inputPassword').val();
+
+    console.log(`Olá ${emailDigitado}, você está tentando logar com a senha ${senhaDigitada}`);
+  });
 });
