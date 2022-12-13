b549564bf9cf8b478fd9044874831a0b1f03d1c5
William Dias <smdwilliam89@gmail.com>
2022-12-13 12:13:23 +0000

    Adicionando o evento de mostar senha

--- js/main.js
+++ js/main.js
@@ -7,4 +7,15 @@ $(document).ready(function () {
 
     console.log(`Olá ${emailDigitado}, você está tentando logar com a senha ${senhaDigitada}`);
   });
+
+  $('#exibir-senha').click(function () {
+    var passwordField = $('#inputPassword');
+    var passwordFieldType = passwordField.attr('type');
+
+    if (passwordFieldType == 'password') {
+      passwordField.attr('type', 'text');
+    } else {
+      passwordField.attr('type', 'password');
+    }
+  });
 });
