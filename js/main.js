8014008c27fe7fa491ab5eb02781fa9fd1988a90
William Dias <smdwilliam89@gmail.com>
2022-12-13 13:58:04 +0000

    Function e IFs

--- js/main.js
+++ js/main.js
@@ -1,38 +1,52 @@
+var listaDeFuncionarios = [
+  {
+    email: 'firstemail@mail.com',
+    password: '123',
+    name: 'John',
+    phone_number: '(353)8333333333',
+    role: 'Developer',
+  },
+  {
+    email: 'secondemail@mail.com',
+    password: '456',
+    name: 'Marie',
+    phone_number: '(353)8333333333',
+    role: 'Manager',
+  },
+  {
+    email: 'thirdemail@mail.com',
+    password: '789',
+    name: 'Willy',
+    phone_number: '(353)8333333333',
+    role: 'Intern',
+  },
+];
+
+function fazerLogin(emailDigitadoNoFormulario, senhaDigitadaNoFormulario) {
+  console.log(`Olá ${emailDigitadoNoFormulario}, você está tentando logar com a senha ${senhaDigitadaNoFormulario}`);
+}
+
 $(document).ready(function () {
-  var listaPaisesObjeto = [
-    {
-      pais: 'Brasil',
-      clima: 'Tropical',
-      perigoso: 'Sim',
-      populacao: '213 milhoes',
-    },
-    {
-      pais: 'Irlanda',
-      clima: 'Frio',
-      perigoso: 'Nao',
-      populacao: '4,94 milhoes',
-    },
-    {
-      pais: 'Itália',
-      clima: 'Tropical',
-      perigoso: 'Não sei',
-      populacao: '60,5 milhoes',
-    },
-  ];
-
-  listaPaisesObjeto.map(function (item, indice) {
-    console.log(item, indice);
-  });
-
   $('#form-login').submit(function (e) {
     e.preventDefault();
 
     var emailDigitado = $('#inputEmail').val();
     var senhaDigitada = $('#inputPassword').val();
 
-    console.log(`Olá ${emailDigitado}, você está tentando logar com a senha ${senhaDigitada}`);
+    // fazerLogin(emailDigitado, senhaDigitada);
+
+    if (emailDigitado == listaDeFuncionarios[0].email) {
+      console.log('Email encontrado na lista 0');
+    } else if (emailDigitado == listaDeFuncionarios[1].email) {
+      console.log('Email encontrado na lista 1');
+    } else if (emailDigitado == listaDeFuncionarios[2].email) {
+      console.log('Email encontrado na lista 2');
+    } else {
+      console.log('Email não encontrado na lista');
+    }
   });
 
+  // EXIBIR SENHA DO FORMULÁRIO
   $('#exibir-senha').click(function () {
     var passwordField = $('#inputPassword');
     var passwordFieldType = passwordField.attr('type');
